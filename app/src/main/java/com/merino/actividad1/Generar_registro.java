735cbb6235828fc26ec48bc5aeb02c7361fac358
thundebolt <nicolasmerino321@gmail.com>
2020-09-28 13:07:57 -0300

    Tercer

--- app/src/main/java/com/merino/actividad1/Generar_registro.java
+++ app/src/main/java/com/merino/actividad1/Generar_registro.java
@@ -2,13 +2,27 @@ package com.merino.actividad1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class Generar_registro extends AppCompatActivity {
+    Button btn_agregar;
+    EditText txt_name,txt_apellido;
+    CheckBox check_edad;
+    RatingBar rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generar_registro);
+        btn_agregar = findViewById(R.id.btn_agregar);
+        txt_name = findViewById(R.id.txt_name);
+        txt_apellido = findViewById(R.id.txt_apellido);
+        check_edad = findViewById(R.id.check_edad);
+        rating = findViewById(R.id.rating);
     }
 }
--- app/src/main/java/com/merino/actividad1/MainActivity.java
+++ app/src/main/java/com/merino/actividad1/MainActivity.java
@@ -2,13 +2,34 @@ package com.merino.actividad1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_generar, btn_ver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    btn_generar = findViewById(R.id.btn_generar);
+    btn_ver = findViewById(R.id.btn_ver);
+
+    btn_generar.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intento1 = new Intent(MainActivity.this,Generar_registro.class);
+            startActivity(intento1);
+        }
+    });
+btn_ver.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent intento2 = new Intent(MainActivity.this,Ver_registro.class);
+        startActivity(intento2);
+    }
+});
     }
 }
