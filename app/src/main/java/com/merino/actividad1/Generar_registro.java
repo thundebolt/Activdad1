3fa6564ca876990a63e464d48e89249bd429eb36
thundebolt <nicolasmerino321@gmail.com>
2020-09-28 12:07:17 -0300

    segunda

---
+++ app/src/main/java/com/merino/actividad1/Generar_registro.java
@@ -0,0 +1,14 @@
+package com.merino.actividad1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Generar_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generar_registro);
+    }
+}
---
+++ app/src/main/java/com/merino/actividad1/Ver_registro.java
@@ -0,0 +1,14 @@
+package com.merino.actividad1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ver_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_registro);
+    }
+}
